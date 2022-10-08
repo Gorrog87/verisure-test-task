9cc1f331bfb806daac93a0e3b06590ce88d108e4
Gorrog <gourguesmaxime@gmail.com>
2022-10-08 12:35:25 +0200

    added FormPage component & Form component

---
+++ src/FormPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./assets/form-page.css";
+import Form from "./Form";
+
+const FormPage = () => {
+  return (
+    <>
+      <div style={{backgroundImage: `url ("../assets/background.jpg")`}}>
+        <Form />
+      </div>
+    </>
+  );
+};
+
+export default FormPage;
